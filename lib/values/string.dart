0f8955b513a14903fc7b4b9f88f2466cb3d086d1
woliur <woliur@sheba.xyz>
2022-06-21 21:21:01 +0600

    [WIP] Basic file arrangements

---
+++ lib/values/string.dart
@@ -0,0 +1,6 @@
+class StringAsset {
+  static const String listTitle = "Repository List";
+  static const String detailsTitle = "Repository Details";
+  static const internetError = "No Internet Found!";
+  static const dataError = "No Data Found!";
+}
